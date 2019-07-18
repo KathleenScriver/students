7c58b010ad8bc643042b5c8e3c6d80a688cfa22a
KathleenScriver <klescriver@gmail.com>
2019-07-18 12:05:41 -0600

    Adds all cohort joining logic.

--- cli.rb
+++ cli.rb
@@ -6,34 +6,58 @@ class Cli
   end
 
   def menu
-    puts "Which cohort(s) do you want to include in the groupings?"
+    puts "Enter the menu number of the cohort(s) you want to include in the grouping.\n"
     if @cohort_3.students.empty?
       list_two_cohort_options
     else
       list_three_cohort_options
     end
-    cohort_choice = gets.chomp.split(' ')
-    puts "Do you want groups of 2, 3, 4, or 5?"
-    group_choice = gets.chomp
-    # if multiple cohorts are selected, need to make a new Cohort object
-    # before running the method
-    cohort_choice.pairs_of(group_choice)
+    cohort_choice = gets.chomp.to_i
+    chosen_cohorts = cohort_map[cohort_choice]
+
+    puts "Do you want groups of 2, 3, 4, or 5?\n"
+    group_choice = gets.chomp.to_i
+
+    student_list = chosen_cohorts.inject([]) do |all_students, cohort|
+      cohort.students.each do |student|
+        all_students.push(student)
+      end
+      all_students
+    end
+
+    cohort_students = Cohort.new(nil, student_list)
+    cohort_students.pairs_of(group_choice)
   end
 
+  private
+
   def list_two_cohort_options
-    puts "1. #{@cohort_1.name}
+    puts "
+    1. #{@cohort_1.name}
     2. #{@cohort_2.name}
-    3. #{@cohort_1.name} and #{@cohort_2.name}
-    4. All Cohorts"
+    4. #{@cohort_1.name} and #{@cohort_2.name}"
   end
 
   def list_three_cohort_options
-    puts "1. #{@cohort_1.name}
+    puts "
+    1. #{@cohort_1.name}
     2. #{@cohort_2.name}
     3. #{@cohort_3.name}
     4. #{@cohort_1.name} and #{@cohort_2.name}
     5. #{@cohort_1.name} and #{@cohort_3.name}
     6. #{@cohort_2.name} and #{@cohort_3.name}
-    7. All Cohorts"
+    7. #{@cohort_1.name} and #{@cohort_2.name} and #{@cohort_3.name}"
+  end
+
+  def cohort_map
+    {
+      1 => [@cohort_1],
+      2 => [@cohort_2],
+      3 => [@cohort_3],
+      4 => [@cohort_1, @cohort_2],
+      5 => [@cohort_1, @cohort_3],
+      6 => [@cohort_2, @cohort_3],
+      7 => [@cohort_1, @cohort_2, @cohort_3]
+    }
   end
 end
--- cohort.rb
+++ cohort.rb
@@ -9,6 +9,7 @@ class Cohort
   def pairs_of(size)
     groups = (students.length / size.to_f).ceil
     shuffled_students = students.shuffle
+    puts "\n**** Your Groups ****\n\n"
     until(shuffled_students.length == 0) do
       if(shuffled_students.length == (size + 1))
         puts "#{shuffled_students.join(' - ')}"
@@ -18,4 +19,5 @@ class Cohort
       puts "#{group.join(' - ')}"
     end
   end
+  puts "\n"
 end
