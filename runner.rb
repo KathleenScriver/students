3f3ad0ca929e3c35b98bfe35b72c5c24e85ad4ea
KathleenScriver <klescriver@gmail.com>
2020-01-06 15:45:43 -0700

    Adds Jan new cohort

--- runner.rb
+++ runner.rb
@@ -19,7 +19,6 @@ bits_please = [
   "Josh",
   "Dani",
   "Alexis",
-  "Rocky",
   "Tissi",
   "Ben",
   "Parker",
@@ -27,22 +26,43 @@ bits_please = [
   "Chuck",
   "Sam",
   "Kelly",
-  "Finn",
   "Patrick",
   "Miriam",
   "John"
 ]
 
-all_students = hashketeers + bits_please
+new_friends = %w(
+  Aaron 
+  AJ 
+  Alice
+  Adam 
+  Brad 
+  Corey 
+  Dillon 
+  Doug 
+  Evan 
+  Jared 
+  Jeremy 
+  Jorge 
+  Lily 
+  Mike 
+  Miwha 
+  Rachel 
+  Todd 
+  Zach-J 
+  Zach-M
+)
+
+all_students = hashketeers + bits_please + new_friends
 
 # Comment in/out code below to get pairs for desired cohort
 # git_mon = Cohort.new("Git Money", git_money)
 
-
+c1 = Cohort.new("New Friends", new_friends)
 
 c2 = Cohort.new("Hashketeers", hashketeers)
 
 c3 = Cohort.new("Bits, Please", bits_please)
 
-cli = Cli.new(c2, c3)
+cli = Cli.new(c2, c3, c1)
 cli.menu
