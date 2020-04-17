0faba7c7f73d3c27adfb094ff37eb4c7ce5fe21a
KathleenScriver <klescriver@gmail.com>
2020-04-17 11:26:59 -0600

    last one

--- runner.rb
+++ runner.rb
@@ -2,44 +2,68 @@ require 'pry'
 require './cohort'
 require './cli'
 
-
-bits_please = [
-  "Josh",
-  "Dani",
-  "Alexis",
-  "Tissi",
-  "Ben",
-  "Parker",
-  "Karl",
-  "Chuck",
-  "Sam",
-  "Kelly",
-  "Patrick",
-  "Miriam",
-  "John"
-]
-
 die_prying = %w(
   Aaron 
-  AJ 
   Alice
   Adam 
   Brad 
   Corey 
   Doug 
-  Evan 
+  Evan
   Jared 
   Jeremy 
-  Jorge 
-  Lily 
+  Jorge
+  Josh 
+  Lilly 
   Mike 
   Miwha 
-  Rachel 
-  Zach-J 
-  Zach-M
+  Tissi
 )
 
-all_students = bits_please + die_prying
+json_derulo = %w(
+  AJ
+  Zach-J 
+  Zach-M
+  Denise
+  Jack
+  Joe
+  Jon
+  JT
+  Katie
+  Kristine
+  Matt
+  Mike
+  Rachel
+  Todd
+  Brandon
+  Nick
+  Tyler
+)
+
+new_friends = %w(
+  Ashley
+  Austin
+  Blake
+  Chris
+  Daniel
+  David F
+  Gregg
+  Hannah
+  Indiana
+  James
+  Jason
+  Justin
+  Katie
+  Lisa
+  Lydia
+  Lynn
+  Nolan
+  Ryan
+  Iuliia
+  Jeremy
+)
+
+all_students = json_derulo + die_prying + new_friends
 
 # Comment in/out code below to get pairs for desired cohort
 # git_mon = Cohort.new("Git Money", git_money)
@@ -47,7 +71,9 @@ all_students = bits_please + die_prying
 c1 = Cohort.new("Die Prying", die_prying)
 
 
-c3 = Cohort.new("Bits, Please", bits_please)
+c2 = Cohort.new("JSON Derulo", json_derulo)
+c3 = Cohort.new("New Friends", new_friends)
 
-cli = Cli.new(c3, c1)
+cli = Cli.new(c1, c2, c3)
 cli.menu
+
